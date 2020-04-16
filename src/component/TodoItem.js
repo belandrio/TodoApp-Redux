73b3e57803bb4b0fd77f00182c9aa67962cb2ac4
bsinoimeri <b.sinoimeri@hotmail.com>
2020-04-16 09:27:25 +0200

    First way of styling

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export class TodoItem extends Component {
   render() {
     return (
-      <div>
+      <div style ={{backgroundColor: "#f4f4f4"}}>
        <p>{this.props.todo.title}</p>
       </div>
     )
--- src/component/Todos.js
+++ src/component/Todos.js
@@ -5,7 +5,7 @@ class Todos extends Component {
     render() {
         return (
             this.props.todos.map((todo) => (
-                <TodoItem  key={todo.id} todo={todo}/>
+                <TodoItem key={todo.id} todo={todo}/>
             )
             )
         )
