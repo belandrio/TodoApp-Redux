89f31bb4faf93fa2cf09d180c82839181f1bc9ca
bsinoimeri <b.sinoimeri@hotmail.com>
2020-04-16 11:32:57 +0200

    From TodoItem to Todo

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -11,18 +11,13 @@ export class TodoItem extends Component {
             textDecoration: this.props.todo.completed ? "line-through" : "none"
         }
     }
-
-markComplete= (e) =>{
-console.log(this.props)
-}
-
-        
+     
 
     render() {
         return (
             <div style={this.getStyle()}>
                 <p> 
-                    <input type="checkbox" onChange={this.markComplete}  /> {' '}
+                    <input type="checkbox" onChange={this.props.markComplete}  /> {' '}
                     {this.props.todo.title} </p>
             </div>
         )
--- src/component/Todos.js
+++ src/component/Todos.js
@@ -3,11 +3,14 @@ import TodoItem from "./TodoItem"
 
 class Todos extends Component {
 
+    markComplete= () =>{
+        console.log("Hello")
+        }
         
     render() {
         return (
             this.props.todos.map((todo) => (
-                <TodoItem key={todo.id} todo={todo}/>
+                <TodoItem key={todo.id} todo={todo} markComplete={this.markComplete}/>
             )
             )
         )
