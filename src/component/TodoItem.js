8270cea6db8d2ec2e7592c742c7255b338c5816b
bsinoimeri <b.sinoimeri@hotmail.com>
2020-04-16 09:28:13 +0200

    Second way of styling

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 
 export class TodoItem extends Component {
-  render() {
-    return (
-      <div style ={{backgroundColor: "#f4f4f4"}}>
-       <p>{this.props.todo.title}</p>
-      </div>
-    )
+    render() {
+        return (
+            <div style={itemStyle}>
+                <p>{this.props.todo.title}</p>
+            </div>
+        )
+    }
 }
+const itemStyle = {
+    backgroundColor: '#f4f4f4'
 }
+
 
 export default TodoItem
